f4719c7e9e718238e4c0e585d8726d76eecb7cd2
JemilCodes <jemiluishaq277@gmail.com>
2022-07-05 00:59:07 +0100

    jemil app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [input1, setInput1] = useState("#00ff00");
+  const [input2, setInput2] = useState("#ffbb00");
+  const [gradient, setGradient] = useState(true);
+
+  const inputPicker = (event) => {
+    setInput1(event.target.value);
+  };
+  const inputPicker2 = (event) => {
+    setInput2(event.target.value);
+  };
+
+  let body = document.getElementById("body");
+
+  body.style.background = `linear-gradient(${
+    gradient ? "to right" : "to left"
+  }, ${input1} ,  ${input2}  )`;
+
+  const styles = {
+    background: `linear-gradient(${
+      gradient ? "to right" : "to left"
+    }, ${input1} ,  ${input2}  )`,
+  };
+
+  const gradientToggler = () => {
+    setGradient((prev) => !prev);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        <h1>BACKGROUND GENERATOR</h1>
       </header>
+      <main>
+        <input
+          type="color"
+          name="color1"
+          onInput={inputPicker}
+          value={gradient ? input1 : input2}
+        />
+        <input
+          type="color"
+          name="color2"
+          onInput={inputPicker2}
+          value={gradient ? input2 : input1}
+        />
+      </main>
+      <section>
+        <div>
+          <h2>CURRENT CSS GRADIENT</h2>
+          <h3>{`linear-gradient(${
+            gradient ? "to right" : "to left"
+          }, ${input1} ,  ${input2}  )`}</h3>
+        </div>
+      </section>
+      <button style={styles} onClick={gradientToggler}>
+        {gradient ? "to left" : "to right"}
+      </button>
     </div>
   );
 }
-
-export default App;
